b932fe25942c9b771afa3a806b1c9829b078c6f2
AxelCh <axel.chuine.pro@gmail.com>
2025-05-10 11:32:16 +0200

    test rating repository est passé

--- src/main/java/com/nnk/springboot/domain/Rating.java
+++ src/main/java/com/nnk/springboot/domain/Rating.java
@@ -31,5 +31,9 @@ public class Rating {
     private Integer orderNumber;
 
     public Rating(String moodysRating, String sandPRating, String fitchRating, int i) {
+        this.moodysRating = moodysRating;
+        this.sandPRating = sandPRating;
+        this.fitchRating = fitchRating;
+        this.orderNumber = i;
     }
 }
