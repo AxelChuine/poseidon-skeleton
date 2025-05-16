81aac1b5a02a98a6bbe36ca211881b9fbb4bb9be
AxelCH <axel.chuine.pro@gmail.com>
2025-05-16 11:12:31 +0200

    ajout de BidListDto

---
+++ src/main/java/com/nnk/springboot/dtos/BidListDto.java
@@ -0,0 +1,35 @@
+package com.nnk.springboot.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BidListDto {
+    private Integer id;
+    private String account;
+    private String type;
+    private Double bidQuantity;
+    private Double askQuantity;
+    private Double bid;
+    private Double ask;
+    private String benchmark;
+    private Timestamp bidListDate;
+    private String commentary;
+    private String security;
+    private String status;
+    private String trader;
+    private String book;
+    private String creationName;
+    private Timestamp creationDate;
+    private String revisionName;
+    private Timestamp revisionDate;
+    private String dealName;
+    private String dealType;
+    private String sourceListId;
+    private String side;
+}
