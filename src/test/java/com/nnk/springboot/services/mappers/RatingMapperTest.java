a179d96d40ef9459b1812f810b29407d86cd0e59
AxelCH <axel.chuine.pro@gmail.com>
2025-05-23 15:07:42 +0200

    test de rating mapper

---
+++ src/test/java/com/nnk/springboot/services/mappers/RatingMapperTest.java
@@ -0,0 +1,76 @@
+package com.nnk.springboot.services.mappers;
+
+import com.nnk.springboot.domain.Rating;
+import com.nnk.springboot.dtos.RatingDto;
+import com.nnk.springboot.services.mapper.RatingMapper;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@ExtendWith(MockitoExtension.class)
+public class RatingMapperTest {
+    @InjectMocks
+    private RatingMapper mapper;
+
+    private final Integer id = 1;
+    private final String moodysRating = "moodysRating";
+    private final String sandPRating = "sandPRating";
+    private final String fitchRating = "fitchRating";
+    private final Integer orderNumber = 1;
+
+    private Rating model;
+    private RatingDto dto;
+    private List<Rating> list = new ArrayList<>();
+    private List<RatingDto> listDto = new ArrayList<>();
+
+    @BeforeEach
+    public void setUp() {
+        this.model = new Rating(
+                this.id,
+                this.moodysRating,
+                this.sandPRating,
+                this.fitchRating,
+                this.orderNumber
+        );
+        this.dto = new RatingDto(
+                this.id,
+                this.moodysRating,
+                this.sandPRating,
+                this.fitchRating,
+                this.orderNumber
+        );
+        this.list.add(this.model);
+        this.listDto.add(this.dto);
+    }
+
+    @Test
+    public void toDtoShouldReturnDto() {
+        RatingDto toCompare = this.mapper.toDto(this.model);
+
+        Assertions.assertThat(toCompare).isEqualTo(dto);
+        Assertions.assertThat(toCompare.toString()).isEqualTo(dto.toString());
+        Assertions.assertThat(toCompare.hashCode()).isEqualTo(dto.hashCode());
+    }
+
+    @Test
+    public void toModelShouldReturnModel() {
+        Rating toCompare = this.mapper.toModel(this.dto);
+
+        Assertions.assertThat(toCompare).isEqualTo(model);
+        Assertions.assertThat(toCompare.toString()).isEqualTo(model.toString());
+        Assertions.assertThat(toCompare.hashCode()).isEqualTo(model.hashCode());
+    }
+
+    @Test
+    public void toDtoListShouldReturnADtoList() {
+        List<RatingDto> toCompare = this.mapper.toDtoList(this.list);
+
+        Assertions.assertThat(toCompare).isEqualTo(listDto);
+    }
+}
