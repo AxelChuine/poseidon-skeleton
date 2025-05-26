cda48e7902dbbe5fc86117e6da591fdae3e38532
AxelCH <axel.chuine.pro@gmail.com>
2025-05-26 14:48:56 +0200

    création de la classe de test du mapper

---
+++ src/test/java/com/nnk/springboot/services/mappers/RuleNameMapperTest.java
@@ -0,0 +1,12 @@
+package com.nnk.springboot.services.mappers;
+
+import com.nnk.springboot.services.mapper.RuleNameMapper;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class RuleNameMapperTest {
+    @InjectMocks
+    private RuleNameMapper mapper;
+}
