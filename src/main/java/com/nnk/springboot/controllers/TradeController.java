bb92b13fa55e87abc8eb2e3efcbfa39fc78d1c41
AxelCH <axel.chuine.pro@gmail.com>
2025-05-27 12:18:59 +0200

    implémentation des méthodes de trade service dans le controller

--- src/main/java/com/nnk/springboot/controllers/TradeController.java
+++ src/main/java/com/nnk/springboot/controllers/TradeController.java
@@ -1,6 +1,9 @@
 package com.nnk.springboot.controllers;
 
 import com.nnk.springboot.domain.Trade;
+import com.nnk.springboot.dtos.TradeDto;
+import com.nnk.springboot.exceptions.ParameterNotProvidedException;
+import com.nnk.springboot.services.impl.TradeService;
 import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,15 +13,23 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+import java.util.Objects;
+
 
 @Controller
 public class TradeController {
-    // TODO: Inject Trade service
+
+    private final TradeService service;
+
+    public TradeController(TradeService service) {
+        this.service = service;
+    }
 
     @RequestMapping("/trade/list")
     public String home(Model model)
     {
-        // TODO: find all Trade, add to model
+        model.addAttribute("trades", this.service.findAll());
         return "trade/list";
     }
 
@@ -28,27 +39,36 @@ public class TradeController {
     }
 
     @PostMapping("/trade/validate")
-    public String validate(@Valid Trade trade, BindingResult result, Model model) {
-        // TODO: check data valid and save to db, after saving return Trade list
+    public String validate(@Valid TradeDto trade, BindingResult result, Model model) throws ParameterNotProvidedException {
+        List<TradeDto> list = this.service.findAll();
+        if (!result.hasErrors()) {
+            list.add(this.service.create(trade));
+            model.addAttribute("trades", list);
+        }
         return "trade/add";
     }
 
     @GetMapping("/trade/update/{id}")
-    public String showUpdateForm(@PathVariable("id") Integer id, Model model) {
-        // TODO: get Trade by Id and to model then show to the form
+    public String showUpdateForm(@PathVariable("id") Integer id, Model model) throws ParameterNotProvidedException {
+        model.addAttribute("trade", this.service.findById(id));
         return "trade/update";
     }
 
     @PostMapping("/trade/update/{id}")
-    public String updateTrade(@PathVariable("id") Integer id, @Valid Trade trade,
-                             BindingResult result, Model model) {
-        // TODO: check required fields, if valid call service to update Trade and return Trade list
+    public String updateTrade(@PathVariable("id") Integer id, @Valid TradeDto trade,
+                             BindingResult result, Model model) throws ParameterNotProvidedException {
+        List<TradeDto> list = this.service.findAll();
+        if (!result.hasErrors() && Objects.nonNull(id)) {
+            list.add(this.service.update(id, trade));
+            model.addAttribute("trades", list);
+        }
         return "redirect:/trade/list";
     }
 
     @GetMapping("/trade/delete/{id}")
-    public String deleteTrade(@PathVariable("id") Integer id, Model model) {
-        // TODO: Find Trade by Id and delete the Trade, return to Trade list
+    public String deleteTrade(@PathVariable("id") Integer id, Model model) throws ParameterNotProvidedException {
+        this.service.deleteById(id);
+        model.addAttribute("trades", this.service.findAll());
         return "redirect:/trade/list";
     }
 }
